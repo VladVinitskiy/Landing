3ea8d1833c46477233a2f8459feecbc7a03ab337
VladVinitskiy <kalitsinskij.46@gmail.com>
2019-02-17 18:16:13 +0200

    Complete version of lending

---
+++ README.md
@@ -0,0 +1,9 @@
+## Descripyion about project
+
+In the project directory you can see the landing of some fictitious companies, which describes some of the company's work, as well as some information about employees and their comments in the form of sliders.
+
+## Deployment
+
+This project has moved here: https://t0-do-list.herokuapp.com/
+
+ool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
---
+++ js/custom.js
@@ -0,0 +1,17 @@
+function menuClick() {
+   document.querySelector('.menuBox').style.display='block';
+   document.querySelector('.menuBox').style.zIndex=1;
+}
+
+function menuClose() {
+    document.querySelector('.menuBox').style.display='none';
+}
+
+function hireUs() {
+    document.querySelector('.hireUs').style.display='flex';
+    document.querySelector('.hireUs').style.zIndex=1;
+}
+
+function hireUsClose() {
+    document.querySelector('.hireUs').style.display='none';
+}
